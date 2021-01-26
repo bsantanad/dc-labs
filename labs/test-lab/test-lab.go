f9695fb57ec7738b26f0b8b7107cb45501914a7c
bsantanad <0211518@up.edu.mx>
2021-01-25 21:06:15 -0600

    Solve test-lab
    Signed-off-by: bsantanad <0211518@up.edu.mx>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+
+	var name string
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Printf("Error\n")
+		return
+	}
+
+	for i, word := range args {
+		name += word
+		if i != (len(args) - 1) {
+			name += " "
+		}
+	}
+	fmt.Printf("Hello %s, Welcome to the jungle\n", name)
 }
