797ee76eb6c6f8df34bbfaad55d899d0cfcec39f
bsanatand <0211518@up.edu.mx>
2021-04-17 17:58:09 -0500

    upload image endpoint now working
    Added new structs for images, one for printing and
    the other for storing.
    
    Signed-off-by: bsanatand <0211518@up.edu.mx>

--- challenges/second-partial/rest-api.go
+++ challenges/second-partial/rest-api.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	//"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -15,15 +16,28 @@ type LoginResponse struct {
 	Token   string `json:"token"`
 }
 
+type Image struct {
+	Name string `json:"name"`
+	Size int    `json:"size"`
+	Data []byte `json:"data"`
+}
+
 type User struct {
-	Username string `json:"user"`
-	Token    string `json:"token"`
-	Time     string `json:"time"`
+	Username string  `json:"user"`
+	Token    string  `json:"token"`
+	Images   []Image `json:"image"`
+	Time     string  `json:"time"`
 }
 
 type Status struct {
 	Message string `json:"message"`
 	Time    string `json:"time"`
+}
+
+type ImageMsg struct {
+	Message  string `json:"message"`
+	Filename string `json:"filename"`
+	Size     string `json:"size"`
 }
 
 type Message struct {
@@ -100,8 +114,67 @@ func delLogout(w http.ResponseWriter, r *http.Request) {
 	returnMsg(w, "Bye "+user.Username+", your token has been revoked")
 }
 
+// based on https://stackoverflow.com/a/40699578
+// postUpload, upload a file (image).
+// It first checks the headers and find the token,
+// validates it and finds the user.
+// Then creates a buffer, copy the bytes of the image
+// to it and fills the Image struct.
+// Finally it append the image to the Image slice
+// the user has.
 func postUpload(w http.ResponseWriter, r *http.Request) {
-	return
+	fmt.Println("[INFO]: POST /upload requested")
+	tmp := r.Header.Get("Authorization")
+	if strings.Fields(tmp)[0] != "Bearer" {
+		w.WriteHeader(400)
+		returnMsg(w, "bad request, check headers "+
+			"you must send a Bearer token")
+		return
+	}
+	token := strings.Fields(tmp)[1] // get the token from header
+	index, user, exists := searchToken(token)
+	if !exists {
+		w.WriteHeader(400)
+		returnMsg(w, "token not found, "+
+			"please provide a valid one")
+		return
+	}
+
+	// uploading the file part
+	r.ParseMultipartForm(32 << 20) // limit your max input length!
+	var buf bytes.Buffer
+	file, header, err := r.FormFile("data")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	// Copy the image data to my buffer
+	io.Copy(&buf, file)
+
+	// Fill the image struct
+	var image Image
+	image.Name = name[0]
+	image.Size = buf.Len()
+	image.Data, err = buf.ReadBytes(254)
+	if err != nil {
+		w.WriteHeader(409)
+		returnMsg(w, "Image couldn't be uploaded :(. Please try again")
+		return
+	}
+	Users[index].Images = append(user.Images, image)
+
+	buf.Reset()
+
+	fmt.Println(image.Size)
+	var msg ImageMsg
+	msg = ImageMsg{
+		Message:  "An image has been successfully uploaded :)",
+		Filename: image.Name,
+		Size:     fmt.Sprintf("%d bytes", image.Size),
+	}
+
+	json.NewEncoder(w).Encode(msg)
 }
 
 // getStatus, show the status of the account related
